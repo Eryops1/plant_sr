73052b972e5404e3d900149058c4a1e2179ea857
Eryops1 <tietje.melanie@gmail.com>
2019-11-08 12:05:40 +0100

    daily update

--- data_to_analysis.R
+++ data_to_analysis.R
@@ -5,6 +5,11 @@ library(rgdal)
 library(rgeos)
 library(raster) 
 
+# data location
+data_location <- "plant_sr_data/"
+# shapefile location
+shapefile_location <- "shapefile/"
+
 # Load data for endemics - phylogenetic
 # load("MRD_ps_endemics.RData")
 # ps_bl_a_end <- ps_bl.a_a_a
@@ -14,15 +19,17 @@ library(raster)
 # rm(MRD.a_a_a, MRD.b_a_a, ps_bl.a_a_a, ps_bl.b_a_a, ps_no.a_a_a, ps_no.b_a_a, comm)
 
 # Load data for all species - phylogenetic
-load("plant_sr_data/MRD_ps.RData")
+load(paste0(data_location, "MRD_ps.RData"))
 ps_bl_a_all <- ps_bl.a_a_a
 ps_bl_b_all <- ps_bl.b_a_a
 ps_no_a_all <- ps_no.a_a_a
 ps_no_b_all <- ps_no.b_a_a
 rm(MRD.a_a_a, MRD.b_a_a, ps_bl.a_a_a, ps_bl.b_a_a, ps_no.a_a_a, ps_no.b_a_a)
 
-# Load data for all species - non-phylogenetic
-load("bs_spp.RData")
+# keep community matrix (comm) and phylosim matrices: ps_bl.a_a_a = phylosim branch length, no= without branch length 
+
+# Load data for all species - non-phylogenetic (non-phylogenetic Simpson dissimilarity based on a community matrix)
+load(paste0(data_location, "bs_spp.RData"))
 
 # Clean data for clustering
 
@@ -51,12 +58,12 @@ rm(exclude, i)
 # Import shapefile for mapping
 
 # load TDWG shapefile
-shape <- readOGR(dsn = "../gis_data/level3",, layer = "level3")
+shape <- readOGR(dsn = shapefile_location, layer = "level3")
 shape <- shape[!shape@data$LEVEL_3_CO=="BOU",]
 
 # project Behrmann
 shape_behr <- spTransform(shape, CRS("+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +datum=WGS84 +ellps=WGS84 +units=m +no_defs"))
-shape_behr@data$area=area(shape_behr)
+  shape_behr@data$area=area(shape_behr)
 
 # islands smaller than Sri Lanka - for visualisation
 area_threshold <- shape_behr@data[shape_behr@data[,"LEVEL_3_CO"]=="SRL","area"]
@@ -64,3 +71,5 @@ area_threshold <- shape_behr@data[shape_behr@data[,"LEVEL_3_CO"]=="SRL","area"]
 shapeCentroids <- gCentroid(shape_behr, byid=TRUE)
 
 shapeBuffer <- gBuffer(shapeCentroids, byid = TRUE, width = 250000)
+
+save.image(paste0(data_location, "data_to_analysis.RData"))
